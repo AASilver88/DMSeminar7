1a4cd7ee9accc015d4e222dc470500dc0ee4b449
AASilcer88 <aasilver74069@gmail.com>
2024-04-23 15:05:13 +0300

    //

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import csv
+import time
 
 #Инициалия драйвера Chrome
 driver = webdriver.Chrome()
@@ -17,16 +19,30 @@ search_box2.submit()
 #Проверяем получили ли какой-нибудь результат
 assert "No results found." not in driver.page_source
 
-#Находим название, артикул и цену запчасти
-spare_part_titles = driver.find_elements(By.CSS_SELECTOR, "span.item-title")
-spare_part_articles = driver.find_elements(By.CSS_SELECTOR, "span.data-article")
-spare_part_prices = driver.find_elements(By.CSS_SELECTOR, "span.data-price")
+# Инициализация пустого списка для хранения цитат
+parts = []
+while True:
+    # Поиск всех цитат на странице с помощью xpath
+    part_elements = driver.find_elements(By.XPATH, '//form[@class="ms2_form"]')
+    # Извлечение текста каждой цитаты
+    for part_element in part_elements:
+        spare_part_title = part_element.find_element(By.XPATH, './/span[@class="item-title wide"]').text
+        spare_part_article = part_element.find_element(By.XPATH, './/span[@class="data-article"]').text
+        spare_part_price = part_element.find_element(By.XPATH, './/span[@class="data-price"]').text
+        parts.append({"spare_part_title": spare_part_title, "spare_part_article": spare_part_article, "spare_part_price": spare_part_price})
+    # Проверка наличия следующей кнопки
+    next_button = driver.find_elements(By.XPATH, '/html/body/div[3]/div/div[2]/div[2]/div[1]/div[7]/div/ul/li[8]/a')
 
-sptitles = [element.text for element in spare_part_titles]
-sparticles = [element.text for element in spare_part_articles]
-spprices = [element.text for element in spare_part_prices]
+    if not next_button:
+        break
+    # Нажатие следующей кнопки
+    next_button[0].click()
+    # Ожидание загрузки страницы
+    time.sleep(1)
 
-for i in range(len(sptitles)):
-    print("{} {} {}".format(sptitles[i], sparticles[i], spprices[i]))
+with open("spare_parts.csv", "w", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=["spare_part_title", "spare_part_article", "spare_part_price"])
+    writer.writeheader()
+    writer.writerows(parts)
 
-driver.quit()
+driver.close()
